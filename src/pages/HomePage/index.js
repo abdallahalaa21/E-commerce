84f3979799a491dac65ae5d1b6a51b983af22716
Abdullah Alaa <abdallah.bodi@gmail.com>
2021-07-11 07:38:58 +0200

    add search to homePage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,26 +1,65 @@
 import ProductCard from 'components/ProductCard';
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import products from 'static/products';
+import React, { useMemo, useState } from 'react';
+import {
+  Col,
+  Container,
+  FormControl,
+  InputGroup,
+  Row
+} from 'react-bootstrap';
+import { ReactComponent as SearchIcon } from 'images/search.svg';
+import useDebounce from 'helpers/useDebounce';
+import productsData from 'static/products';
 
-const HomePage = () => (
-  <Container className="mt-4">
-    <Row>
-      {products.map(product => (
-        <Col
-          xs={12}
-          sm={6}
-          md={6}
-          lg={4}
-          xl={4}
-          key={product.id}
-          className="mb-3"
-        >
-          <ProductCard product={product} />
-        </Col>
-      ))}
-    </Row>
-  </Container>
-);
+const HomePage = () => {
+  const [searchTxt, setSearchTxt] = useState('');
+  const debouncedSearchTerm = useDebounce(searchTxt, 500);
+
+  const products = useMemo(
+    () =>
+      productsData.filter(product =>
+        product.title
+          .toLowerCase()
+          .includes(debouncedSearchTerm.toLowerCase())
+      ),
+    [debouncedSearchTerm]
+  );
+
+  return (
+    <Container className="mt-4">
+      <Row>
+        <InputGroup className="mb-3 w-50 mx-auto">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="search">
+              <SearchIcon height="100%" width="20px" />
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="search products"
+            aria-label="search products"
+            aria-describedby="search"
+            value={searchTxt}
+            onChange={e => setSearchTxt(e.target.value)}
+          />
+        </InputGroup>
+      </Row>
+      <Row>
+        {products.map(product => (
+          <Col
+            xs={12}
+            sm={6}
+            md={6}
+            lg={4}
+            xl={4}
+            key={product.id}
+            className="mb-3"
+          >
+            <ProductCard product={product} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 
 export default HomePage;
