20de1e741c5f9d9bf04e59c89426ab5550108834
Abdullah Alaa <abdallah.bodi@gmail.com>
2021-07-12 02:35:23 +0200

    finish adding list toggle

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -5,26 +5,33 @@ import {
   Col,
   Container,
   FormControl,
+  Image,
   InputGroup,
-  Row
+  Row,
+  Button,
+  Table
 } from 'react-bootstrap';
 
 import { ReactComponent as SearchIcon } from 'images/search.svg';
+import { ReactComponent as ListIcon } from 'images/list.svg';
+import { ReactComponent as GridIcon } from 'images/grid.svg';
+
 import ProductCard from 'components/ProductCard';
 import { searchProduct } from 'redux/Products/products-actions';
 import useDebounce from 'helpers/useDebounce';
+import { NavLink } from 'react-router-dom';
 
 const HomePage = ({ products, searchProductFunc }) => {
   const [searchTxt, setSearchTxt] = useState('');
+  const [isList, setIsList] = useState(false);
   const debouncedSearchTerm = useDebounce(searchTxt, 500);
   useEffect(
     () => searchProductFunc(debouncedSearchTerm),
     [searchProductFunc, debouncedSearchTerm]
   );
-
   return (
     <Container className="mt-4">
-      <Row>
+      <Row className="mb-3">
         <InputGroup className="mb-3 w-50 mx-auto">
           <InputGroup.Prepend>
             <InputGroup.Text id="search">
@@ -39,21 +46,69 @@ const HomePage = ({ products, searchProductFunc }) => {
             onChange={e => setSearchTxt(e.target.value)}
           />
         </InputGroup>
+        <Button
+          onClick={() => setIsList(true)}
+          className="mr-3"
+          variant={isList ? 'primary' : 'secondary'}
+        >
+          <ListIcon
+            style={{ width: '30px', fill: 'white' }}
+          />
+        </Button>
+        <Button
+          onClick={() => setIsList(false)}
+          variant={!isList ? 'primary' : 'secondary'}
+        >
+          <GridIcon
+            style={{ width: '30px', fill: 'white' }}
+          />
+        </Button>
       </Row>
       <Row>
-        {products?.map(product => (
-          <Col
-            xs={12}
-            sm={6}
-            md={6}
-            lg={4}
-            xl={4}
-            key={product.id}
-            className="mb-3"
-          >
-            <ProductCard product={product} />
-          </Col>
-        ))}
+        {isList ? (
+          <Table responsive>
+            <tbody>
+              {products.map(product => (
+                <NavLink
+                  className="navLink"
+                  to={`${product?.id}`}
+                >
+                  <tr key={product.id}>
+                    <td>
+                      <Image
+                        src={product.image}
+                        width="100px"
+                      />
+                    </td>
+                    <td>
+                      <tr>{product.title}</tr>
+                      <tr>{product.description}</tr>
+                    </td>
+                    <td>{product.category}</td>
+                    <td>{product.price}</td>
+                  </tr>
+                </NavLink>
+              ))}
+            </tbody>
+          </Table>
+        ) : (
+          <>
+            {' '}
+            {products?.map(product => (
+              <Col
+                xs={12}
+                sm={6}
+                md={6}
+                lg={4}
+                xl={4}
+                key={product.id}
+                className="mb-3"
+              >
+                <ProductCard product={product} />
+              </Col>
+            ))}
+          </>
+        )}
       </Row>
     </Container>
   );
